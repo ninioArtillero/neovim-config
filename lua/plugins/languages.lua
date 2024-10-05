cd053a50c2c7ab796903064b6c18d323b6fd34e0
Xavier Góngora <ixbalanque@protonmail.ch>
2024-10-05 12:25:42 -0600

    add my programming languages
    Used defaults from LazyVim extra packages when available. They set
    tree-sitter, lsp and other goodies. For other languages I set them
    manually.

---
+++ lua/plugins/languages.lua
@@ -0,0 +1,31 @@
+return {
+
+  { import = "lazyvim.plugins.extras.lang.haskell" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.lang.nix" },
+  { import = "lazyvim.plugins.extras.lang.scala" },
+
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        fish_lsp = {},
+        scheme_langserver = {},
+      },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
+        "fish",
+        "supercollider",
+      })
+    end,
+  },
+}
