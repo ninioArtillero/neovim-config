ec5aa88e95af170a2ae4b7a2a5e1a2024ad85ccd
Xavier Góngora <ixbalanque@protonmail.ch>
2024-10-07 17:11:18 -0600

    git, yaml and recommended lazy extras

--- lua/plugins/languages.lua
+++ lua/plugins/languages.lua
@@ -1,10 +1,12 @@
 return {
 
+  { import = "lazyvim.plugins.extras.lang.git" },
   { import = "lazyvim.plugins.extras.lang.haskell" },
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.markdown" },
   { import = "lazyvim.plugins.extras.lang.nix" },
   { import = "lazyvim.plugins.extras.lang.scala" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
 
   {
     "neovim/nvim-lspconfig",
--- lua/plugins/lazy-extras.lua
+++ lua/plugins/lazy-extras.lua
@@ -1,3 +1,5 @@
 return {
   { import = "lazyvim.plugins.extras.ui.mini-animate" },
+  { import = "lazyvim.plugins.extras.util.dot" },
+  { import = "lazyvim.plugins.extras.coding.yanky" },
 }
